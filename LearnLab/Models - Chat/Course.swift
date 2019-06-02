1936c378ab1ee5e8ad1dea27cefa2950fd825738
Siddharth Kucheria <kucheria@usc.edu>
2019-06-02 16:04:55 -0700

    basic search queries work. need to update tableview with results

---
+++ LearnLab/Models - Chat/Course.swift
@@ -0,0 +1,18 @@
+//
+//  Course.swift
+//  LearnLab
+//
+//  Created by Siddharth Kucheria on 6/2/19.
+//  Copyright © 2019 Siddharth Kucheria. All rights reserved.
+//
+
+import UIKit
+
+@objcMembers
+class Course: NSObject {
+    var code: String?
+    var department: String?
+    var school : String?
+    var title : String?
+}
+
--- LearnLab/ViewControllers/SearchVC.swift
+++ LearnLab/ViewControllers/SearchVC.swift
@@ -14,6 +14,11 @@ class SearchVC: UITableViewController, UISearchBarDelegate {
     
     lazy var searchBar:UISearchBar = UISearchBar()
 
+    var fstore : Firestore!
+    
+    var filteredData = [Course]()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,11 +35,35 @@ class SearchVC: UITableViewController, UISearchBarDelegate {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        
+        fstore = Firestore.firestore()
+        
+//        pullCourses()
 
+    }
+    
+    func pullCourses(_ text : String){
+        fstore?.collection("courses").whereField("department", isEqualTo: "CSCI").getDocuments(completion: { (snapshot, error) in
+            
+            for doc in snapshot!.documents{
+                if let dictionary = doc.data() as? [String:String]{
+                    let course = Course()
+                    course.code = dictionary["code"]
+                    course.school = dictionary["school"]
+                    course.department = dictionary["department"]
+                    course.title = dictionary["title"]
+                    print(course.title)
+                    self.filteredData.append(course)
+                }
+            }
+        })
     }
 
     func searchBar(_ searchBar: UISearchBar, textDidChange textSearched: String)
     {
+        //query for courses and update the table viiew
+        pullCourses(textSearched)
         //sercah bar works
     }
     
@@ -90,48 +119,5 @@ class SearchVC: UITableViewController, UISearchBarDelegate {
     }
 
     
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return false
-    }
-    
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
