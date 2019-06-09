6234791a1519a23fe97c48cb76b97e32fb6e2c0d
Siddharth Kucheria <kucheria@usc.edu>
2019-06-09 01:34:36 -0700

    Basic setup for 2nd tutor reg VC done. need to add search bar and table view now

--- LearnLab/Tutor Registration/Registration2VC.swift
+++ LearnLab/Tutor Registration/Registration2VC.swift
@@ -7,13 +7,42 @@
 //
 
 import UIKit
+import Firebase
 
 class Registration2VC: UIViewController {
 
+    var ref : DatabaseReference?
+    
+    let doneButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Done", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(donePressed), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
+        ref = Database.database().reference()
+        
+        self.view.addSubview(doneButton)
+        setupDoneButton()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func setupDoneButton(){
+        doneButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        doneButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10).isActive = true
+        doneButton.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        doneButton.heightAnchor.constraint(equalToConstant: 20).isActive = true
+    }
+    
+    @objc func donePressed(){
+    
+    
     }
     
 
