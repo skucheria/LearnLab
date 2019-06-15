042826878f4623c28504ec33be3e51796645cffe
Siddharth Kucheria <kucheria@usc.edu>
2019-06-15 14:47:39 -0700

    minor ui changes

--- LearnLab/ViewControllers/SessionsVC.swift
+++ LearnLab/ViewControllers/SessionsVC.swift
@@ -14,31 +14,60 @@ class SessionsVC: UIViewController {
     let sessionSegment : UISegmentedControl = {
         let segment = UISegmentedControl(items: ["Current", "Past"])
         segment.translatesAutoresizingMaskIntoConstraints = false
-        segment.tintColor = .white
+        segment.tintColor = .blue
         segment.selectedSegmentIndex = 1
-//        segment.addTarget(self, action: #selector(segChanged), for: .valueChanged )
+        segment.addTarget(self, action: #selector(segChanged), for: .valueChanged )
         segment.isUserInteractionEnabled = true
         return segment
     }()
     
+    let topView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let sessionsTV : UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(sessionSegment)
         self.view.backgroundColor = .gray
         self.title = "Sessions"
         setupSegment()
-        
+        setupTableView()
         // Do any additional setup after loading the view.
     }
     
     func setupSegment(){
+        self.view.addSubview(topView)
         let barHeight = 2 *  (self.navigationController?.navigationBar.frame.height)!
-//        sessionSegment.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        topView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        topView.topAnchor.constraint(equalTo: view.topAnchor, constant: barHeight).isActive = true
+        topView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        topView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        topView.addSubview(sessionSegment)
         sessionSegment.topAnchor.constraint(equalTo: view.topAnchor, constant: barHeight).isActive = true
         sessionSegment.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         sessionSegment.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         sessionSegment.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
     
+    func setupTableView(){
+        self.view.addSubview(sessionsTV)
+        sessionsTV.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        sessionsTV.topAnchor.constraint(equalTo: sessionSegment.bottomAnchor).isActive = true
+        sessionsTV.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        sessionsTV.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    }
+    
+    @objc func segChanged(){
+        
     }
     /*
     // MARK: - Navigation
